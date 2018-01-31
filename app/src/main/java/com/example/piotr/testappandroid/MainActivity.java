4bb8b5d459788509749ad69809e16f216deee00f
Piotr Bala <piotr.baa@gmail.com>
2018-01-31 21:28:23 +0100

    Starting creating first method in MainActivity.java and changes in layout - need to refactor

--- app/src/main/java/com/example/piotr/testappandroid/MainActivity.java
+++ app/src/main/java/com/example/piotr/testappandroid/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.piotr.testappandroid;
 
+import android.app.Activity;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void findingSomething(View view) {
+        TextView smth = (TextView) findViewById(R.id.textViewID);
+        smth.setText("Created new text");
     }
 }
